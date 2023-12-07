6e626c3350141c0af5072871670cb79903b43a61
Hiren <rebeldevmurari@gmail.com>
2023-12-07 13:09:13 +0000

    added more functions to as per requirements

--- src/main/java/com/rebel/Ecom/Butter_sb_API/Models/User.java
+++ src/main/java/com/rebel/Ecom/Butter_sb_API/Models/User.java
@@ -1,11 +1,13 @@
 package com.rebel.Ecom.Butter_sb_API.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
